3ddbcd1661440ef721fda628d4b25881ec1cd0c7
jobin jose <jobinjose.in@gmail.com>
2022-11-07 18:05:42 +0530

    Java Workout-2

--- Problem7/InstanceTracker.java
+++ Problem7/InstanceTracker.java
@@ -22,7 +22,6 @@ public class InstanceTracker {
         t1 = null;
         //call garbage collector.
         System.gc();
-//        Runtime.getRuntime().gc();
         //display Instances created, destroyed and live information.
         System.out.println(String.format("Instances created: %d", createdCounter));
         System.out.println(String.format("Instances live: %d", liveCounter));
